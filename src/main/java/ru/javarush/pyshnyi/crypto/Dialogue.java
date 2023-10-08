41731884056deaf4eb7736a704c2aa9adace6027
Eugene Pyshnyi <pinch15975@gmail.com>
2023-10-08 22:00:57 +0500

    Project is ready

--- src/main/java/ru/javarush/pyshnyi/crypto/Dialogue.java
+++ src/main/java/ru/javarush/pyshnyi/crypto/Dialogue.java
@@ -7,21 +7,25 @@ import java.util.Scanner;
 public class Dialogue {
     public static void run(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Выберите режим работы программы:");
-        System.out.println("1. Encryption \n2. Decryption \n3. Brute Force decryption \n");
-        int i = scanner.nextInt();
-        switch (i) {
-            case 1 :
-                Encode.encode();
-                break;
-            case 2 :
-                Decode.decode();
-                break;
-            case 3 :
-                BruteForce.bruteForce();
-                break;
-            default :
-                System.err.println("Выберите один из вариантов");
-        }
+        int choice;
+
+        do {
+            System.out.println("Выберите режим работы программы:");
+            System.out.println("1. Encryption \n2. Decryption \n3. Brute Force decryption \n");
+            choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    Encode.encode();
+                    break;
+                case 2:
+                    Decode.decode();
+                    break;
+                case 3:
+                    BruteForce.bruteForce();
+                    break;
+                default:
+                    System.err.println("Выберите один из вариантов");
+            }
+        } while (choice < 1 || choice > 3);
     }
 }
