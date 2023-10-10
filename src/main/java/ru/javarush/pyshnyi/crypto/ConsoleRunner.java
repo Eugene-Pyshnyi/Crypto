8828f62f488264ec0ace5a8cd8ad6854e8a691d6
Eugene Pyshnyi <pinch15975@gmail.com>
2023-10-10 19:56:02 +0500

    Committed

--- src/main/java/ru/javarush/pyshnyi/crypto/ConsoleRunner.java
+++ src/main/java/ru/javarush/pyshnyi/crypto/ConsoleRunner.java
@@ -1,6 +1,7 @@
 package ru.javarush.pyshnyi.crypto;
 
 public class ConsoleRunner {
+    public static String PRIVET_ANTON;
     public static void main(String[] args) {
         Dialogue.run();
     }
